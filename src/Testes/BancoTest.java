4c4623e2c37fc676ac7a19d19ee8691b220f8ae0
Camila <13180211@W1975051.portoalegre.pucrsnet.br>
2016-08-30 22:04:29 -0300

    Testes na classe Banco

--- src/Testes/BancoTest.java
+++ src/Testes/BancoTest.java
@@ -14,31 +14,35 @@ import Main.Cliente;
 public class BancoTest {
 
 	private Banco banco;
-	   private Cliente cli;
-	   private Caixa caixa;
-	    
-	    @Before
-	    public void setUp() {
-	        banco = new Banco();
-	    }
-	    
-	     /*@Test
-	    public void testGetAtendimento1() {
-	        banco.cadastrar("Kim", 65);
-	        banco.proximoCliente();
-	        int actual = caixa.getAtendendo();
-	        assertEquals(1, actual);
-	    }*/
-	    
-	    @Test
-	    public void testAddCaixas(){
-	    banco.addCaixas(5);
-	    assertEquals(5, banco.getCaixas().size());
-	    }
-	    
-//	    @Test
-//	    public void testCadastrar(){
-//	    
-//	    }
+	private Cliente cliente;
+	private Caixa caixa;
+
+	@Before
+	public void setUp() {
+		banco = new Banco();
+	}
+
+//	@Test
+//	public void testGetAtendimento1() {
+//		banco.cadastrar("Kim", 65);
+//		banco.proximoCliente();
+//		int actual = caixa.getAtendendo();
+//		assertEquals(1, actual);
+//	}
+
+	@Test
+	public void testAddCaixas() {
+		banco.addCaixas(5);
+		assertEquals(5, banco.getCaixas().size());
+	}
+
+	@Test
+	public void testCadastrar() {
+		cliente = new Cliente("Camila", 23);
+		String clienteNome = cliente.getNome();
+		int idadeCliente = cliente.getIdade();
+		assertEquals("Camila", clienteNome);
+		assertEquals(23, idadeCliente);
+	}
 
 }
